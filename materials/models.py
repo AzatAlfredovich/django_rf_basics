3975d68304e296f82669ddb514b8f682c19808b3
Azat Khaliullin <AllisonUno@yandex.ru>
2025-12-01 23:54:35 +0400

    Созданы модели, подключен DRF, описаны CRUD (для курса - Viewsets, для урока - Generic-классы, описаны сериализаторы, проведены проверки в Postman

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+
+# Курс
+class Course(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Название курса",
+        help_text="Введите название курса",
+    )
+
+    image = models.ImageField(
+        upload_to="materials/photo",
+        blank=True,
+        null=True,
+        verbose_name="Превью",
+        help_text="Загрузите изображение",
+    )
+
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание",
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return f"Курс '{self.name}'"
+
+
+# Урок
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Название урока",
+        help_text="Введите название урока",
+    )
+
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание",
+    )
+
+    image = models.ImageField(
+        upload_to="avatars/",
+        blank=True,
+        null=True,
+        verbose_name="Превью",
+        help_text="Загрузите изображение",
+    )
+
+    video = models.URLField(
+        blank=True,
+        null=True,
+        verbose_name="Видео",
+        help_text="Введите ссылку на урок",
+    )
+
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        verbose_name="Курс",
+        help_text="Выберите курс",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return f"Урок '{self.name}'"
