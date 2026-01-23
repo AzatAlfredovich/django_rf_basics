0ab7bf055d96e684740c329f6eee3aa0df272901
Azat Khaliullin <AllisonUno@yandex.ru>
2026-01-23 22:30:47 +0400

    Добавлена директория staticfiles

--- users/permissions.py
+++ users/permissions.py
@@ -11,6 +11,18 @@ class IsModerator(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.groups.filter(name="moderators").exists()
 
+class IsNotModerator(permissions.BasePermission):
+    """
+    Проверка, что пользователь НЕ входит в группу «moderators»
+    """
+    message = "Доступ запрещён: вы являетесь модератором"
+
+    def has_permission(self, request, view):
+        # Возвращаем True, если пользователь НЕ в группе "moderators"
+        return not request.user.groups.filter(name="moderators").exists()
+
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
 
 class IsOwner(permissions.BasePermission):
     """
